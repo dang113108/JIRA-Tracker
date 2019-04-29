fbd1ea422d0e896cd33796dd651bd9c8102940d2
exosite-RobertChen <robertchen@exosite.com>
2019-04-29 18:15:04 +0800

    Hotfix for saving minutes will become hour (Issue: JIRA-030)

--- js/popup.js
+++ js/popup.js
@@ -2,7 +2,7 @@ var countSec = 0;
 var isRecord = false;
 var isPause = false;
 var isStop = false;
-var version = "v0.1.1";
+var version = "v0.1.1h";
 var saveData = ['img', 'startTime', 'countSec', 'issue', 'timeSpent', 'comment', 'isRecord', 'isPause', 'isStop', 'account', 'password'];
 var removeData = ['startTime', 'countSec', 'isRecord', 'isPause', 'isStop'];
 var removeUIData = ['img', 'issue', 'timeSpent', 'comment'];
@@ -223,8 +223,8 @@ $(function() {
         comment = $("#comment").val();
 
         timeSpent = timeSpent.substring(0, timeSpent.length - 1);
-        timeSpent = timeSpent.split("h");
-        if (timeSpent.indexOf("h")) {
+        if (timeSpent.indexOf("h") != -1) {
+            timeSpent = timeSpent.split("h");
             if (timeSpent.length == 2) {
                 totalTime = timeSpent[0] * 3600 + timeSpent[1] * 60;
             } else {
@@ -305,9 +305,9 @@ $(function() {
         });
     });
 
-    $("#versionText").on('click', function() {
-        chrome.tabs.create({ url: "https://github.com/dang113108/JIRA-Tracker"});
-    });
+$("#versionText").on('click', function() {
+    chrome.tabs.create({ url: "https://github.com/dang113108/JIRA-Tracker"});
+});
 
 });
 
